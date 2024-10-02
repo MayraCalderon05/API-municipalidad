3177b63288e5710f18a6cc939c632ef22f6b2895
MayraCalderon05 <may2005_cc@hotmail.com>
2024-10-02 12:55:56 -0300

    confirmacion API

---
+++ core/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database {
+    private $host = "172.16.20.30";
+    private $db_name = "eventos_municipales";
+    private $username = "desarrollo";
+    private $password = "fisca1234";
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+
+?>
