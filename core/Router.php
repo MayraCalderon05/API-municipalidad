9257a2ee52acc520c1d570abfba72b79924afd00
AgusXDeath <gonzacofreesc@gmail.com>
2024-10-03 11:51:17 -0300

    Redireccioné el servicio a la api en vez de los archivos

--- core/Router.php
+++ core/Router.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../controllers/EventoController.php';
+include_once '../controllers/eventoController.php';
 include_once '../views/Vi.php';
 
 $controller = new EventoController();
