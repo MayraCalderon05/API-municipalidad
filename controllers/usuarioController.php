b2e11e519e253c1f188a296e0d5c416454a51ac4
MayraCalderon05 <may2005_cc@hotmail.com>
2024-10-06 16:26:41 -0300

    User controller terminado

---
+++ controllers/usuarioController.php
@@ -0,0 +1,68 @@
+<?php
+    header("Access-Conttelefono-Allow-Origin: *");  // Permite todas las fuentes
+    header("Access-Conttelefono-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");  // Métodos permitidos
+    header("Access-Conttelefono-Allow-Headers: Content-Type, Authorization");  // Encabezados permitidos
+
+    include_once '../core/Database.php';
+    include_once '../models/Usuario.php';
+
+    class UsuarioController {
+        private $db;
+        private $usuario;
+
+        public function __construct() {
+            $database  = new Database();
+            $this->db = $database->getConnection();
+            $this->usuario = new Usuario($this->db);
+        }
+
+        //obtener  todos los usuarios desde el meyodo del modelo
+        public function getAllUsers(){
+            $stmc = $this->usuario->getAllUsers();
+            $usuarios = $stmc->fetchALL(PDO::FETCH_ASSOC);
+            return json_encode($usuarios);
+        }
+
+        //obtener usuario por ID desde el metodo del modelo Usuario
+        public function getUserById($id){
+            $usuario = $this->usuario->getUserById($id);
+            return json_encode($usuario);
+        }
+
+        //crear un usuario
+        public function  createUser($data){
+            $this->usuario->nombre = $data->nombre;
+            $this->usuario->apellido = $data->apeliido;
+            $this->usuario->email = $data->email;
+            $this->usuario->password = $data->password;
+            $this->usuario->telefono = $data->telefono;
+            if ($this->usuario->createUser()) {
+                return json_encode(["message" => "Usuario creado con éxito"]);
+            }
+            return json_encode(["message" => "Error al crear el usuario"]);
+        }
+
+        //actualizar un usuario
+        public function updateUser($id, $data){
+            $this->usuario->nombre = $data->nombre;
+            $this->usuario->apellido = $data->apeliido;
+            $this->usuario->email = $data->email;
+            $this->usuario->password = $data->password;
+            $this->usuario->telefono = $data->telefono;
+            if ($this->usuario->updateUser($id)) {
+                return json_encode(["message" => "Usuario actualizado con éxito"]);
+            }
+            return json_encode(["message"=>"Ha habido un error al actualizar el usuario"]);
+        }
+
+        //eliminar un usuario
+        public function deleteUsuario($id){
+            if ($this->usuario->deleteUsuario($id)) {
+                return  json_encode(["message" => "Usuario eliminado con éxito"]);
+            }
+            return  json_encode(["message" => "Ha habido un error al eliminar el usuario"]);
+        }
+
+    }
+
+?>
--- core/Router.php
+++ core/Router.php
@@ -1,8 +1,10 @@
 <?php
 include_once '../controllers/eventoController.php';
+include_once '../controllers/usuarioController.php'
 include_once '../views/View.php';
 
 $controller = new EventoController();
+$userController = new UsuarioController();
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
@@ -11,6 +13,42 @@ switch ($method) {
             $result = $controller->getById($_GET['uid']);
         } else {
             $result = $controller->getAll();
+        }
+        View::render($result);
+        break;
+
+    case 'POST':
+        $data = json_decode(file_get_contents("php://input"));
+        $result = $controller->create($data);
+        View::render($result);
+        break;
+
+    case 'PUT':
+        if (isset($_GET['uid'])) {
+            $data = json_decode(file_get_contents("php://input"));
+            $result = $controller->update($_GET['uid'], $data);
+            View::render($result);
+        }
+        break;
+
+    case 'DELETE':
+        if (isset($_GET['uid'])) {
+            $result = $controller->delete($_GET['uid']);
+            View::render($result);
+        }
+        break;
+
+    default:
+        View::render(json_encode(["message" => "Método no permitido"]));
+        break;
+}
+//switch de los usuarios
+switch ($method) {
+    case 'GET':
+        if (isset($_GET['id'])) {
+            $result = $userController->getUserById($_GET['id']);
+        } else {
+            $result = $userController->getAllUsers();
         }
         View::render($result);
         break;
